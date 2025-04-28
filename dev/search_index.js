b751b592befee19b813462f55849096adc71e3cd
Documenter.jl <documenter@juliadocs.github.io>
2025-04-28 11:12:11 +0000

    build based on 23e09c1

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#SciBmad","page":"Home","title":"SciBmad","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for SciBmad.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
+}
