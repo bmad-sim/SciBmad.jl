ad27ef5cdf12eab4a9c7c413f74f7062c2f74379
mattsignorelli <mgs255@cornell.edu>
2026-01-20 16:58:52 -0500

    many changes to twiss, including 'at' and tunes now (breaking)

--- src/twiss.jl
+++ src/twiss.jl
@@ -1,3 +1,8 @@
+struct Twiss{S,T}
+  tunes::S
+  table::T
+end
+
 # Returns a Table of the Twiss parameters
 # See Eq. 4.28 in EBB
 function twiss(
@@ -6,6 +11,7 @@ function twiss(
   de_moivre=false,
   co_info=find_closed_orbit(bl),
   symplectic_tol=1e-8, # Tolerance below which to include damping
+  at::Union{Colon,AbstractArray}=:, # Colon means all elements, nothing means no elements
 )
 
   if isnothing(GTPSA_descriptor)
@@ -61,47 +67,93 @@ function twiss(
   track!(b0, bl)
   m = DAMap(nv=nv, np=np, v=view(dropdims(b0.coords.v; dims=1), 1:nv))
 
-  # Check if symplectic or not
-  damping = norm(NNF.checksymp(NNF.jacobian(m))) > symplectic_tol
+  # twiss will ALWAYS compute the (amplitude-dependent) tunes, even when `at` is empty
+  # function barrier:
+  tunes, a = _tunes_and_a(m, mo, coast)
 
-  if mo > 1 && (coast || nn > 6)
-    zero_LF = TI.init_tps(numtype, init)
+  if at isa Colon || (at isa AbstractArray && length(at) > 0)
+    if !(at isa Colon)
+      if eltype(at) != LineElement
+        error("Keyword argument `at` must be either a Colon (:) to specify all elements, 
+                an empty array (which can have Any type) to specify no elements, or an 
+                array with element type LineElement.")
+      else
+        at = sort(at, by=x->x.beamline_index)
+      end
+    end
+
+    # Check if symplectic or not
+    damping = norm(NNF.checksymp(NNF.jacobian(m))) > symplectic_tol
+
+    if mo > 1 && (coast || nn > 6)
+      zero_LF = TI.init_tps(numtype, init)
+    else
+      zero_LF = zero(numtype)
+    end
+
+    # Type of the PHASES
+    if coast || mo > 1 && nn > 6
+      zero_phase = TI.init_tps(numtype, init)
+    else
+      zero_phase = zero(numtype)
+    end
+    
+    # Type of the ORBIT
+    if coast || nn > 6
+      zero_orbit = TI.init_tps(numtype, init)
+    else
+      zero_orbit = zero(numtype)
+    end
+
+    N_ele = at isa Colon ? length(bl.line) : length(at)
+    # Fill the s array now for each at
+    stmp = Vector{Any}(undef, N_ele)
+    scur = 0f0
+    idx = 1
+    for ele in bl.line
+      if at isa Colon || ele in at
+        stmp[idx] = scur
+        idx += 1
+      end
+      scur += ele.L
+    end
+    s = typeof(scur).(stmp)
+    lf_table = _twiss(a, b0, bl, s, Val{de_moivre}(), damping, zero_LF, zero_phase, zero_orbit, at)
   else
-    zero_LF = zero(numtype)
+    lf_table = nothing
   end
 
-  # Type of the PHASES
-  if coast || mo > 1 && nn > 6
-    zero_phase = TI.init_tps(numtype, init)
-  else
-    zero_phase = zero(numtype)
-  end
-  
-  # Type of the ORBIT
-  if coast || nn > 6
-    zero_orbit = TI.init_tps(numtype, init)
-  else
-    zero_orbit = zero(numtype)
-  end
+  return Twiss(tunes, lf_table)
+end
 
-  # Type of the s coordinate
-  zero_s = zero(bl.line[end].s)
-
-  # function barrier
-  return _twiss(m, b0, bl, Val{de_moivre}(), damping, zero_LF, zero_phase, zero_orbit, zero_s)
+function _tunes_and_a(m::DAMap, mo, coast)
+  a = normal(m)
+  c = c_map(m) # Transform to phasor basis
+  r = inv(c) ∘ inv(a) ∘ m ∘ a ∘ c
+  # Need to cut highest order
+  Q_x = cutord(real(-log(SciBmad.NNF.factor_out(r.v[1], 1))/(2*pi*im)), mo)
+  Q_y = cutord(real(-log(SciBmad.NNF.factor_out(r.v[3], 3))/(2*pi*im)), mo)
+  if coast
+    Q_s = real(r.v[5])
+    TI.seti!(Q_s, 0, 5) # subtract time identity
+  else
+    Q_s = cutord(real(-log(SciBmad.NNF.factor_out(r.v[5], 5))/(2*pi*im)), mo)
+  end
+  return SA[Q_x, Q_y, Q_s], a
 end
   
 function _twiss(
-  m::DAMap, 
+  a::DAMap, 
   b0::Bunch, 
   bl::Beamline, 
+  s,
   ::Val{de_moivre}, 
   damping,
   zero_LF::T, 
   zero_phase::V,
   zero_orbit::U, 
-  zero_s::S
-) where {de_moivre, T, V, U, S}
+  at::C,
+) where {de_moivre, T, V, U, C}
   # Ripken-Wolski-Forest de Moivre coupling formalism
   # If linear is true, then we do not need to do any factorization
   # Else we must factorize at every element
@@ -140,34 +192,49 @@ function _twiss(
     PROCESS_ORBIT = v -> StaticArrays.sacollect(SVector{6,U}, TI.scalar(v[i]) for i in 1:6)
   end
   # =================================================================
+  if C == Colon
+    N_ele = length(bl.line)
+  else
+    N_ele = length(at)
+  end
 
-  s::S = zero_s
-  N_ele = length(bl.line)
-  a = normal(m)
-  NNF.setray!(a.v, scalar=m.v)
+  # a = normal(m)
+  # This should not be necessary anymore with fixing of normal:
+  # NNF.setray!(a.v, scalar=m.v)
   r = canonize(a, SCALAR_PHASE; damping=damping)
   a = a ∘ r
   a0, a1, a2 = factorize(a)
   NNF_tuple = COMPUTE_TWISS(a1, SCALAR_LF)
-  lf1 = LF(S(s), SA[zero(zero_phase),zero(zero_phase),zero(zero_phase)], NNF_tuple, PROCESS_ORBIT(a0.v))
+  lf1 = LF(zero(first(s)), SA[zero(zero_phase),zero(zero_phase),zero(zero_phase)], NNF_tuple, PROCESS_ORBIT(a0.v))
   lf_table = LF_TABLE(lf1, N_ele)
+
+  idx = 1
+  if C == Colon || bl.line[1] in at
+    lf_table[1] = lf1
+    idx += 1
+    if idx > N_ele
+      return lf_table
+    end
+  end
+
   phase = MVector{3}(zero(zero_phase),zero(zero_phase),zero(zero_phase))
-  for i in 1:N_ele
-    if SCALAR_PHASE isa Val{true}
-      phase .= 0
-    else
-      TI.clear!(phase[1]); TI.clear!(phase[2]); TI.clear!(phase[3]);
-    end 
+  len = length(bl.line)
+  for i in 1:(len-1)
     b0.coords.v .= view(a.v, 1:6)'
     track!(b0, bl.line[i])
     NNF.setray!(a.v; v=view(b0.coords.v, 1:6))
-    s = lf_table.s[i] + S(bl.line[i].L)::S
+    #s = lf_table.s[i] + S(bl.line[i].L)::S
     r = canonize(a, SCALAR_PHASE; phase=phase, damping=damping)
     a = a ∘ r
-    a0, a1, a2 = factorize(a)
-    old_phases = SA[lf_table.phi_1[i], lf_table.phi_2[i], lf_table.phi_3[i]]
-    lfi = LF(s, old_phases+phase, COMPUTE_TWISS(a1, SCALAR_LF), PROCESS_ORBIT(a0.v))
-    lf_table[i+1] = lfi
+    if C == Colon || bl.line[i] in at
+      a0, a1, a2 = factorize(a)
+      lfi = LF(s[idx], SA[copy(phase[1]), copy(phase[2]), copy(phase[3])], COMPUTE_TWISS(a1, SCALAR_LF), PROCESS_ORBIT(a0.v))
+      lf_table[idx] = lfi
+      idx += 1
+      if idx > N_ele
+        break
+      end
+    end
   end
   return lf_table
 end
@@ -239,60 +306,60 @@ function twiss_table(tt, N_ele)
   U = typeof(tt.orbit_x)
   if haskey(tt, :eta_1)
     t = Table(
-      s = Vector{S}(undef, N_ele+1),
-      phi_1 = Vector{V}(undef, N_ele+1),
-      beta_1 = Vector{T}(undef, N_ele+1),
-      alpha_1 = Vector{T}(undef, N_ele+1),
-      phi_2 = Vector{V}(undef, N_ele+1),
-      beta_2 = Vector{T}(undef, N_ele+1),
-      alpha_2 = Vector{T}(undef, N_ele+1),
-      phi_3 = Vector{V}(undef, N_ele+1),
-      eta_1   = Vector{T}(undef, N_ele+1),
-      etap_1  = Vector{T}(undef, N_ele+1),
-      eta_2   = Vector{T}(undef, N_ele+1),
-      etap_2  = Vector{T}(undef, N_ele+1),
-      zeta_1  = Vector{T}(undef, N_ele+1),
-      zetap_1 = Vector{T}(undef, N_ele+1),
-      zeta_2  = Vector{T}(undef, N_ele+1),
-      zetap_2 = Vector{T}(undef, N_ele+1),
-      slip    = Vector{T}(undef, N_ele+1),
-      gamma_c = Vector{T}(undef, N_ele+1),
-      c11 = Vector{T}(undef, N_ele+1),
-      c12 = Vector{T}(undef, N_ele+1),
-      c21 = Vector{T}(undef, N_ele+1),
-      c22 = Vector{T}(undef, N_ele+1),
-      orbit_x = Vector{U}(undef, N_ele+1),
-      orbit_px = Vector{U}(undef, N_ele+1),
-      orbit_y = Vector{U}(undef, N_ele+1),
-      orbit_py = Vector{U}(undef, N_ele+1),
-      orbit_z = Vector{U}(undef, N_ele+1),
-      orbit_pz = Vector{U}(undef, N_ele+1),
+      s = Vector{S}(undef, N_ele),
+      phi_1 = Vector{V}(undef, N_ele),
+      beta_1 = Vector{T}(undef, N_ele),
+      alpha_1 = Vector{T}(undef, N_ele),
+      phi_2 = Vector{V}(undef, N_ele),
+      beta_2 = Vector{T}(undef, N_ele),
+      alpha_2 = Vector{T}(undef, N_ele),
+      phi_3 = Vector{V}(undef, N_ele),
+      eta_1   = Vector{T}(undef, N_ele),
+      etap_1  = Vector{T}(undef, N_ele),
+      eta_2   = Vector{T}(undef, N_ele),
+      etap_2  = Vector{T}(undef, N_ele),
+      zeta_1  = Vector{T}(undef, N_ele),
+      zetap_1 = Vector{T}(undef, N_ele),
+      zeta_2  = Vector{T}(undef, N_ele),
+      zetap_2 = Vector{T}(undef, N_ele),
+      slip    = Vector{T}(undef, N_ele),
+      gamma_c = Vector{T}(undef, N_ele),
+      c11 = Vector{T}(undef, N_ele),
+      c12 = Vector{T}(undef, N_ele),
+      c21 = Vector{T}(undef, N_ele),
+      c22 = Vector{T}(undef, N_ele),
+      orbit_x = Vector{U}(undef, N_ele),
+      orbit_px = Vector{U}(undef, N_ele),
+      orbit_y = Vector{U}(undef, N_ele),
+      orbit_py = Vector{U}(undef, N_ele),
+      orbit_z = Vector{U}(undef, N_ele),
+      orbit_pz = Vector{U}(undef, N_ele),
     )
-    t[1] = tt
+    # t[1] = tt
     return t
   else
     t = Table(
-      s = Vector{S}(undef, N_ele+1),
-      phi_1 = Vector{V}(undef, N_ele+1),
-      beta_1 = Vector{T}(undef, N_ele+1),
-      alpha_1 = Vector{T}(undef, N_ele+1),
-      phi_2 = Vector{V}(undef, N_ele+1),
-      beta_2 = Vector{T}(undef, N_ele+1),
-      alpha_2 = Vector{T}(undef, N_ele+1),
-      phi_3 = Vector{V}(undef, N_ele+1),
-      gamma_c = Vector{T}(undef, N_ele+1),
-      c11 = Vector{T}(undef, N_ele+1),
-      c12 = Vector{T}(undef, N_ele+1),
-      c21 = Vector{T}(undef, N_ele+1),
-      c22 = Vector{T}(undef, N_ele+1),
-      orbit_x = Vector{U}(undef, N_ele+1),
-      orbit_px = Vector{U}(undef, N_ele+1),
-      orbit_y = Vector{U}(undef, N_ele+1),
-      orbit_py = Vector{U}(undef, N_ele+1),
-      orbit_z = Vector{U}(undef, N_ele+1),
-      orbit_pz = Vector{U}(undef, N_ele+1),
+      s = Vector{S}(undef, N_ele),
+      phi_1 = Vector{V}(undef, N_ele),
+      beta_1 = Vector{T}(undef, N_ele),
+      alpha_1 = Vector{T}(undef, N_ele),
+      phi_2 = Vector{V}(undef, N_ele),
+      beta_2 = Vector{T}(undef, N_ele),
+      alpha_2 = Vector{T}(undef, N_ele),
+      phi_3 = Vector{V}(undef, N_ele),
+      gamma_c = Vector{T}(undef, N_ele),
+      c11 = Vector{T}(undef, N_ele),
+      c12 = Vector{T}(undef, N_ele),
+      c21 = Vector{T}(undef, N_ele),
+      c22 = Vector{T}(undef, N_ele),
+      orbit_x = Vector{U}(undef, N_ele),
+      orbit_px = Vector{U}(undef, N_ele),
+      orbit_y = Vector{U}(undef, N_ele),
+      orbit_py = Vector{U}(undef, N_ele),
+      orbit_z = Vector{U}(undef, N_ele),
+      orbit_pz = Vector{U}(undef, N_ele),
     )
-    t[1] = tt
+    # t[1] = tt
     return t
   end
 end
@@ -322,21 +389,21 @@ function de_moivre_table(dt, N_ele)
   T = typeof(dt.H)
   U = typeof(dt.orbit_x)
   t = Table(
-    s = Vector{S}(undef, N_ele+1),
-    phi_1 = Vector{V}(undef, N_ele+1),
-    phi_2 = Vector{V}(undef, N_ele+1),
-    phi_3 = Vector{V}(undef, N_ele+1),
-    H = Vector{T}(undef, N_ele+1),
-    B = Vector{T}(undef, N_ele+1),
-    E = Vector{T}(undef, N_ele+1),
-    K = Vector{T}(undef, N_ele+1),
-    orbit_x = Vector{U}(undef, N_ele+1),
-    orbit_px = Vector{U}(undef, N_ele+1),
-    orbit_y = Vector{U}(undef, N_ele+1),
-    orbit_py = Vector{U}(undef, N_ele+1),
-    orbit_z = Vector{U}(undef, N_ele+1),
-    orbit_pz = Vector{U}(undef, N_ele+1),
+    s = Vector{S}(undef, N_ele),
+    phi_1 = Vector{V}(undef, N_ele),
+    phi_2 = Vector{V}(undef, N_ele),
+    phi_3 = Vector{V}(undef, N_ele),
+    H = Vector{T}(undef, N_ele),
+    B = Vector{T}(undef, N_ele),
+    E = Vector{T}(undef, N_ele),
+    K = Vector{T}(undef, N_ele),
+    orbit_x = Vector{U}(undef, N_ele),
+    orbit_px = Vector{U}(undef, N_ele),
+    orbit_y = Vector{U}(undef, N_ele),
+    orbit_py = Vector{U}(undef, N_ele),
+    orbit_z = Vector{U}(undef, N_ele),
+    orbit_pz = Vector{U}(undef, N_ele),
   )
-  t[1] = dt
+  # t[1] = dt
   return t
 end
