d65d94a1e4879bf110df70874cd84005b83f73ff
mattsignorelli <mgs255@cornell.edu>
2026-02-23 16:19:31 -0500

    fix

--- ext/SciBmadGPUArraysCoreExt/SciBmadGPUArraysCoreExt.jl
+++ ext/SciBmadGPUArraysCoreExt/SciBmadGPUArraysCoreExt.jl
@@ -4,14 +4,14 @@ import DifferentiationInterface as DI
 using GPUArraysCore: @allowscalar, AbstractGPUArray
 using LinearAlgebra: Transpose
 
-function DI.basis(a::Transpose{<:AbstractGPUArray{T}}, i) where {T}
+function DI.basis(a::Transpose{T,<:AbstractGPUArray{T}}, i) where {T}
     b = similar(a)
     fill!(b, zero(T))
     @allowscalar b[i] = oneunit(T)
     return b
 end
 
-function DI.multibasis(a::Transpose{<:AbstractGPUArray{T}}, inds) where {T}
+function DI.multibasis(a::Transpose{T,<:AbstractGPUArray{T}}, inds) where {T}
     b = similar(a)
     fill!(b, zero(T))
     view(b, inds) .= oneunit(T)
