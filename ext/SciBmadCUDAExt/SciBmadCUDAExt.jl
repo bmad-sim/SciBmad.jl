3cf39b68bb9bae49589482fa0c915ce8d548e017
mattsignorelli <mgs255@cornell.edu>
2026-02-23 16:14:51 -0500

    add GPUArraysCore ext workaround

--- ext/SciBmadCUDAExt/SciBmadCUDAExt.jl
+++ ext/SciBmadCUDAExt/SciBmadCUDAExt.jl
@@ -3,6 +3,4 @@ using CUDA
 using CUDSS # Sparse CUDA matrix solving on the device
 import SciBmad
 
-SciBmad.device_specific_sparse(m, device::CUDA.CUDABackend) = CUSPARSE.CuSparseMatrixCSR(m)
-
 end
