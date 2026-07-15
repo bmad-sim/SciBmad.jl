2a9609044c0abfb79bfd85056b1286d9ef85b9c7
mattsignorelli <mgs255@cornell.edu>
2026-07-15 11:44:10 -0400

    begin docs update

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -123,6 +123,7 @@ html_theme_options = {
 html_title = 'SciBmad.jl Documentation'
 html_static_path = ['_static']
 html_css_files = ['custom.css']
+html_logo = '_static/scibmad.pdf'
 
 # Sidebar settings with custom external links
 html_sidebars = {
