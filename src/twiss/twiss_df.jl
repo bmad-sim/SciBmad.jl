a532520c597b13ff0c728a7e561a2975a574b3f4
mattsignorelli <mgs255@cornell.edu>
2026-08-23 10:07:35 -0400

    ready to go

--- src/twiss/twiss_df.jl
+++ src/twiss/twiss_df.jl
@@ -13,14 +13,14 @@ function _twiss_df(colnames, twi, include_start, include_end, ::Val{as_taylor_se
   map_cache = build_cache(typeof(twi.fac[1].a))
   tps_cache = build_cache(typeof(first(twi.fac[1].a.v)))
   float_cache = build_cache(Float64)
-  smatrix4_cache = build_cache(SMatrix{4,4,Float64})
-  smatrix6_cache = build_cache(SMatrix{6,6,Float64})
+  matrix_cache = build_cache(Matrix{Float64})
   vf_cache = build_cache(complex(typeof(zero(VectorField, twi.fac[1].a))))
 
+  persistent_matrix_cache = build_cache(Matrix{Float64})
   persistent_map_cache = build_cache(typeof(twi.fac[1].a))
   persistent_cmap_cache = build_cache(complex(typeof(twi.fac[1].a)))
 
-  cache = TwissCache(map_cache, tps_cache, float_cache, smatrix4_cache, smatrix6_cache, vf_cache, persistent_map_cache, persistent_cmap_cache)
+  cache = TwissCache(map_cache, tps_cache, float_cache, matrix_cache, vf_cache, persistent_matrix_cache, persistent_map_cache, persistent_cmap_cache)
   for i in 1:ncols
     col = cols[i]
     row1[i] = @noinline col(1, twi, cache, Val{as_taylor_series}())
@@ -52,8 +52,7 @@ function _twiss_df_loop(df, cols, twi, cache, shift, ::Val{as_taylor_series}) wh
       empty!(cache.map)
       empty!(cache.tps)
       empty!(cache.float)
-      empty!(cache.smatrix4)
-      empty!(cache.smatrix6)
+      empty!(cache.matrix)
       empty!(cache.vf)
     end
   end
